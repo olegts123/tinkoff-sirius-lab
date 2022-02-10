01bccbf27e4f0f47252d3d32d30114697f25b231
Oleg Tsemenko <tsemenko.me@gmail.com>
2022-02-10 22:04:28 +0300

    add title and companyName labels

--- tinkoff-sirius-lab/ViewController.swift
+++ tinkoff-sirius-lab/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var companyNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        companyNameLabel.text = "Tinkoff"
     }
 
 
