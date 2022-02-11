a612e93df630a45dcf75b794f67cc7d3d0a82b00
Oleg Tsemenko <tsemenko.me@gmail.com>
2022-02-11 14:19:44 +0300

    setup company picker

--- tinkoff-sirius-lab/ViewController.swift
+++ tinkoff-sirius-lab/ViewController.swift
@@ -7,18 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
 
     @IBOutlet weak var companyNameLabel: UILabel!
     @IBOutlet weak var companyPickerView: UIPickerView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     
+    // MARK - private properties
+    
+    private let companies: [String: String] = ["Apple": "AAPL",
+                                               "Microsoft": "MSFT",
+                                               "Google": "GOOG",
+                                               "Amazon": "AMZN",
+                                               "Facebook": "FB"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        companyNameLabel.text = "Tinkoff"
+        self.companyNameLabel.text = "Tinkoff"
+        
+        self.companyPickerView.dataSource = self
+        
+        self.companyPickerView.delegate = self
     }
 
-
+    
+    
+    // MARK - UIPickerViewDataSource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.companies.keys.count
+    }
+    
+    //MARK - UIPickerViewDelegate
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Array(self.companies.keys)[row]
+    }
 }
 
